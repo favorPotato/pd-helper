8ff18a01c4ec56637be65ee40af46df87c5ddfd6
favorPotato <1487483572z@gmail.com>
2026-05-29 06:28:07 +0800

    chore(release): v0.9.0

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,7 +1,7 @@
 export default {
     manifest_version: 3,
     name: 'pd-helper',
-    version: '0.8.6',
+    version: '0.9.0',
     description: 'instagram 运营助手',
     incognito: 'split',
     permissions: [
