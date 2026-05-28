c800088ca58537ad582548c7ecef3104a2add140
favorPotato <1487483572z@gmail.com>
2026-05-28 18:28:51 +0800

    chore(release): v0.7.0

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,7 +1,7 @@
 export default {
     manifest_version: 3,
     name: 'ig_helper',
-    version: '0.6.0',
+    version: '0.7.0',
     description: 'instagram 运营助手',
     permissions: [
         'scripting',
